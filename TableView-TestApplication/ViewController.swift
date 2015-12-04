1eae4eab03f9a4ab93adb19ce32ae09c0c0fcede
Robert Byrne <mr.rob.byrne@gmail.com>
2015-12-04 04:40:39 -0330

    Table view and configuration
    This commit was to create a Table View and populate it with the icon data.
    
    It is all building and running but there is 1 issue...
    the icon images are not showing up. I have tried to find the reason for this but have had no success.

--- TableView-TestApplication/ViewController.swift
+++ TableView-TestApplication/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var icons = [Icon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let iconSets = IconSet.iconSets()
+        let iconSet = iconSets[0]
+        icons = iconSet.icons
+        
+        // removes space before first row of table
+        automaticallyAdjustsScrollViewInsets = false
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +32,28 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController : UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return icons.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("IconCell", forIndexPath: indexPath)
+        let icon = icons[indexPath.row]
+        
+        cell.textLabel?.text = icon.title
+        cell.detailTextLabel?.text = icon.subtitle
+        
+        if let imageView = cell.imageView, iconImage = icon.image {
+            imageView.image = iconImage
+        }
+
+        
+        return cell
+    }
+    
 }
 
