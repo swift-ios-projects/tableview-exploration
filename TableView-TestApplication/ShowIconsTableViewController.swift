ec6a82cfcdd056f61ac1f426827998f7cd6e30a2
Robert Byrne <mr.rob.byrne@gmail.com>
2015-12-05 03:04:32 -0330

    Fixed: Images now showing up in app
    # What was fixed
    
    The images are now showing up in the current build of the app. They were missing up until this point.
    
    # How was it fixed
    
    The images had to be changed to group (yellow folder), whereas they were originally reference folders (blue folder)

---
+++ TableView-TestApplication/ShowIconsTableViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ShowIconsTableViewController.swift
+//  TableView-TestApplication
+//
+//  Created by Robert Byrne on 2015-12-05.
+//  Copyright © 2015 LeanRob. All rights reserved.
+//
+
+import UIKit
+
+class ShowIconsTableViewController: UITableViewController {
+
+    var icons = [Icon]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Initialize iconSet from iconsets Iconsets.swift, then set a variable to be the first set of icons store them in icons variable
+        let iconSets = IconSet.iconSets()
+        let iconSet = iconSets[0]
+        icons = iconSet.icons
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return icons.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("IconCell", forIndexPath: indexPath)
+
+        let icon = icons[indexPath.row]
+        cell.textLabel?.text = icon.title
+        cell.detailTextLabel?.text = icon.subtitle
+        if let iconImage = icon.image {
+            cell.imageView?.image = iconImage
+        }
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
